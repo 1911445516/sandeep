903ffd6ac6c888bae3b43701d0a13626a24b6b74
1911445516 <35676573+1911445516@users.noreply.github.com>
2018-01-31 09:43:50 +0530

    Add files via upload

---
+++ vijay.py
@@ -0,0 +1,16 @@
+print("sandeep")
+a=12
+b=10
+c=a+b
+print(c)
+print('vijay kumar')
+a=input("enter a's value")
+b=input("enter b's value")
+c=a+b
+print(c)
+a=int(input("enter a's value"))
+b=int(input("enter b's value"))
+c=a+b
+print(c)
+
+
