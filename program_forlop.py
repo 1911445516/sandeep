d59224e029ba6b8006db32bf5d405ff53285067e
1911445516 <35676573+1911445516@users.noreply.github.com>
2018-02-19 09:35:26 +0530

    Add files via upload

---
+++ program_forlop.py
@@ -0,0 +1,7 @@
+
+for a in range(1,32,3):
+    print(a)
+for i in [1,2,3,4,5]:
+    if(i==3):
+        break
+    print(i)
---
+++ program_fun2.py
@@ -0,0 +1,14 @@
+x=14
+y=33
+def add(x,y):
+  return x+y
+def sub(x,y):
+  return x-y
+def mul(x,y):
+  return x*y
+def div(x,y):
+  return x/y
+print(add(x,y))
+print(sub(x,y))
+print(mul(x,y))
+print(div(x,y))
